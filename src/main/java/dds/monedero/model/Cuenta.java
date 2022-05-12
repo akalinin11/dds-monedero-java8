c3203922fe7c9ffd8b0fdfc125b475eb6df82a53
Alexander Kalinin <akalinin@frba.utn.edu.ar>
2022-05-12 15:46:24 -0300

    Code Smell: message chains y corregimos constructor

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -14,6 +14,13 @@ public class Cuenta {
   private double saldo = 0;
   private List<Movimiento> movimientos = new ArrayList<>();
 
+  // Esto reemplaza a los constructores de "cuenta" y al setMovimientos
+  public Cuenta(Double saldo, List<Movimiento> movimientos){
+    this.saldo = saldo;
+    this.movimientos = movimientos;
+  }
+
+  /*
   public Cuenta() {
     saldo = 0;
   }
@@ -25,18 +32,26 @@ public class Cuenta {
   public void setMovimientos(List<Movimiento> movimientos) {
     this.movimientos = movimientos;
   }
+*/
+
+
 
   public void poner(double cuanto) {
-    if (cuanto <= 0) {
+    if (cuanto <= 0){
       throw new MontoNegativoException(cuanto + ": el monto a ingresar debe ser un valor positivo");
     }
-
-    if (getMovimientos().stream().filter(movimiento -> movimiento.isDeposito()).count() >= 3) {
+    //Code Smell corregido: Message Chains
+    if(this.cantidadDepositos() >= 3) {
       throw new MaximaCantidadDepositosException("Ya excedio los " + 3 + " depositos diarios");
     }
-
     new Movimiento(LocalDate.now(), cuanto, true).agregateA(this);
   }
+
+
+  public Long cantidadDepositos(){
+      return movimientos.stream().filter(Movimiento::isDeposito).count();
+  } //No falta usar la funcion del getter de movimientos cuando estas dentro de la clase
+
 
   public void sacar(double cuanto) {
     if (cuanto <= 0) {
