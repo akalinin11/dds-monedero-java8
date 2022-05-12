31e53561f1ea6a777272fdfb9255e53884e3b347
Alexander Kalinin <akalinin@frba.utn.edu.ar>
2022-05-12 16:47:37 -0300

    Agregamos abstraccion de concretarMovimiento

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -16,7 +16,7 @@ public class Cuenta {
   private List<Movimiento> movimientos = new ArrayList<>();
 
   // Esto reemplaza a los constructores de "cuenta" y al setMovimientos
-  public Cuenta(Double saldo, List<Movimiento> movimientos){
+  public Cuenta(Double saldo, List<Movimiento> movimientos) {
     this.saldo = saldo;
     this.movimientos = movimientos;
   }
@@ -33,24 +33,22 @@ public class Cuenta {
   public void setMovimientos(List<Movimiento> movimientos) {
     this.movimientos = movimientos;
   }
-*/
-
-
+  */
 
   public void poner(double cuanto) {
-    if (cuanto <= 0){
+    if (cuanto <= 0) {
       throw new MontoNegativoException(cuanto + ": el monto a ingresar debe ser un valor positivo");
     }
     //Code Smell corregido: Message Chains
-    if(this.cantidadDepositos() >= 3) {
+    if (this.cantidadDepositosDeUnaFecha(LocalDate.now()) >= 3) {
       throw new MaximaCantidadDepositosException("Ya excedio los " + 3 + " depositos diarios");
     }
-    new Movimiento(LocalDate.now(), cuanto, true).agregateA(this);
+    this.concretarMovimiento(cuanto, true);
   }
 
 
-  public Long cantidadDepositos(){
-      return movimientos.stream().filter(Movimiento::isDeposito).count();
+  public Long cantidadDepositosDeUnaFecha(LocalDate fecha) {
+    return movimientos.stream().filter(movimiento -> movimiento.fueDepositado(fecha)).count();
   } //No falta usar la funcion del getter de movimientos cuando estas dentro de la clase
 
 
@@ -67,16 +65,15 @@ public class Cuenta {
     double limite = 1000 - montoExtraidoHoy;
     */
     if (cuanto > limiteRestante()) {
-      throw new MaximoExtraccionDiarioException("No puede extraer mas de $ " + 1000
+      throw new MaximoExtraccionDiarioException("No puede extraer mas de $ " + limiteDiario
           + " diarios, límite: " + limiteRestante());
     }
-
-    new Movimiento(LocalDate.now(), cuanto, false).agregateA(this);
+    this.concretarMovimiento(cuanto, false);
   }
 
   double limiteDiario = 1000;
 
-  public double limiteRestante(){
+  public double limiteRestante() {
     return limiteDiario - getMontoExtraidoA(LocalDate.now());
   }
 
@@ -89,7 +86,7 @@ public class Cuenta {
     return extraccionesDeUnaFecha(fecha).mapToDouble(Movimiento::getMonto).sum();
   } // Code Smell: Long method
 
-  public Stream<Movimiento> extraccionesDeUnaFecha(LocalDate fecha){
+  public Stream<Movimiento> extraccionesDeUnaFecha(LocalDate fecha) {
     return movimientos.stream().filter(movimiento -> movimiento.fueExtraido(fecha));
   } // Code Smell: Duplicated code
 
@@ -105,4 +102,14 @@ public class Cuenta {
     this.saldo = saldo;
   }
 
+  public void concretarMovimiento(double cuanto, boolean esDeposito) {
+    if(esDeposito){
+      new Movimiento(LocalDate.now(), cuanto, true).agregateA(this);
+    }else {
+      new Movimiento(LocalDate.now(), cuanto, false).agregateA(this);
+    }
+  }
 }
+
+
+
